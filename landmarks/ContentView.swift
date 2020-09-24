1ac8fb52343f93682c94bbab6d648fbf1a65716d
Juan Carlo Magat <magatjuancarlo@gmail.com>
2020-09-23 20:26:48 -0700

    Initial Commit

---
+++ landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  landmarks
+//
+//  Created by Juan Carlo on 9/23/20.
+//  Copyright © 2020 Juan Carlo Magat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
