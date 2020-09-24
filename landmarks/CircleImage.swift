45f6eabbb95b6edcd09bea52f2fefca6db6a0b7c
Juan Carlo Magat <magatjuancarlo@gmail.com>
2020-09-23 20:57:18 -0700

    Set up basic UI

---
+++ landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by Juan Carlo on 9/23/20.
+//  Copyright © 2020 Juan Carlo Magat. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
